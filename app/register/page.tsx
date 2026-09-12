4afdc782c70ecf97b9aed2a8141267b439e03fcc
lizozivzivadze-star <lizozivzivadze@gmail.com>
2026-09-12 14:11:56 +0200

    Update 12-Sep-26 14:11:56.16

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -75,7 +75,9 @@ export default function RegisterPage() {
             </label>
             <input
               id="email"
+              name="email"
               type="email"
+              autoComplete="email"
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
